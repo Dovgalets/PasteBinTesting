7e7726be77bcc627428748c73e28e26a0a339641
SMD <smdov@i.ua>
2020-02-22 09:59:26 +0200

    For Jenkins #3 headless mode with proxy

--- src/test/java/Abctractions/PasteBin.java
+++ src/test/java/Abctractions/PasteBin.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.concurrent.TimeUnit;
+
 public abstract class PasteBin {
     static {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
@@ -20,16 +22,17 @@ public abstract class PasteBin {
 //        this();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("enable-automation");
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-extensions");
         options.addArguments("--dns-prefetch-disable");
         options.addArguments("--disable-gpu");
         options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        Proxy proxy = new Proxy();
-        proxy.setHttpProxy("proxy.lan:3128");
-        options.setProxy(proxy);
+//        Proxy proxy = new Proxy();
+//        proxy.setHttpProxy("proxy.lan:3128");
+//        options.setProxy(proxy);
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         PageFactory.initElements(driver, this);
         driver.manage().window().maximize();
         driver.get(url);
